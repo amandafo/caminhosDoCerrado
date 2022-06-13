01e9fce38e001061b2337e7148d0273685abbcb3
Amanda Ferreira <amandafo2308@gmail.com>
2022-06-13 03:02:15 -0300

    :feat: Start versioning api

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,16 @@ module Caminhocerradoapi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origin '*'
+        resource '*',
+          headers: :any,
+          methods: %i(get post put patch delet option head)
+      end
+    end
+
+    config.middleware.use Rack::Attack
   end
 end
